d6712d75497bb728616ee68acf7c928ec8d354fe
Mychel Albuquerque <94093354+MychelAlbuquerque@users.noreply.github.com>
2022-09-08 16:54:21 -0300

    resolve requisito 1 (#89)

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
+  const url = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const response = await url.json();
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const response = await url.json();
+  return response;
 }
 
 export async function getProductById() {
