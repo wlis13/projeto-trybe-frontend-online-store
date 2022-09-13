bc9f46f52946350ccf178f5742834043a3531949
Mychel Albuquerque <mychelalbu@gmail.com>
2022-09-12 21:09:38 -0300

    resolve requisito 9

--- src/pages/productDetails.jsx
+++ src/pages/productDetails.jsx
@@ -18,6 +18,19 @@ class ProductDetails extends Component {
     });
   }
 
+  setProductsStorage = () => {
+    const products = JSON.parse(localStorage.getItem('products')) || [];
+    const { Nomes, Price, Imagen } = this.state;
+    const name = Nomes;
+    const price = Price;
+    const image = Imagen;
+    const objectProduct = { name, price, image, quantity: 1 };
+
+    localStorage
+      .setItem('products', JSON.stringify([...products, objectProduct]));
+    console.log(name);
+  };
+
   render() {
     const { Nomes, Price, Imagen } = this.state;
     return (
@@ -26,6 +39,14 @@ class ProductDetails extends Component {
         <h3 data-testid="product-detail-name">{ Nomes }</h3>
         <img src={ Imagen } alt="imagem do produto" data-testid="product-detail-image" />
         <h4 data-testid="product-detail-price">{` R$${Price} `}</h4>
+        <button
+          data-testid="product-detail-add-to-cart"
+          type="button"
+          onClick={ this.setProductsStorage }
+        >
+          Adicionar ao carrinho
+
+        </button>
       </div>
     );
   }
